a1a51851f100ef1cd803d98e2af9e186c5160bd2
Roman <roman@roman.com>
2022-09-16 09:04:07 +0300

    Changes to be committed: 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.css 	new file:   client/src/components/SideBar/SideBar.jsx 	new file:   client/src/components/SideBar/SideBar.module.css 	new file:   client/src/components/SideBar/SideBarItem.jsx 	new file:   client/src/components/SideBar/SideBarItem.module.css 	modified:   client/src/pages/AdminPanel.jsx 	modified:   client/src/routes.js 	new file:   client/src/utils/constsAdminPanel.js

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,10 +1,13 @@
 import {React} from "react";
+import SideBar from "../components/SideBar/SideBar";
 
 const AdminPanel = () => {
 
     return (
         <div>
-            
+            <SideBar>
+                fgdfg
+            </SideBar>
         </div>
     )
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,13 @@
 import {
     MAIN_ROUTE,
-    CONFIGURED_ROUTE
+    CONFIGURED_ROUTE,
+    ADMIN_ROUTE
 } from "./utils/consts";
 
 
 import Main from "./pages/Main.jsx";
 import Configured from "./pages/Configured.jsx";
+import AdminPanel from "./pages/AdminPanel";
 
 
 export const publickRoutes = [
@@ -17,4 +19,15 @@ export const publickRoutes = [
         path: CONFIGURED_ROUTE,
         Component: Configured
     },
+    {
+        path: ADMIN_ROUTE,
+        Component: AdminPanel
+    },
+]
+
+
+
+
+export const adminPanelRoutes = [
+    
 ]
