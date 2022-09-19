d04ab542e7d9ed4afcc52f3f8b54f3e5a519d63e
Roman <roman@roman.com>
2022-09-19 11:07:20 +0300

    Changes to be committed: 	deleted:    client/src/components/SideBar/SideBar.jsx 	deleted:    client/src/components/SideBar/SideBar.module.css 	deleted:    client/src/components/SideBar/SideBarItem.jsx 	deleted:    client/src/components/SideBar/SideBarItem.module.css 	deleted:    client/src/components/adminPanelItem/AdminPanelItem.jsx 	deleted:    client/src/components/adminPanelItem/AdminPanelItem.module.css 	deleted:    client/src/components/formInput.jsx 	deleted:    client/src/components/listItem.jsx 	deleted:    client/src/components/oneItem.jsx 	deleted:    client/src/components/test/formInput.jsx 	deleted:    client/src/components/test/listItem.jsx 	deleted:    client/src/components/test/oneItem.jsx 	deleted:    client/src/components/test/test.jsx 	modified:   client/src/pages/AdminPanel.jsx 	modified:   client/src/pages/Main.jsx

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,57 +1,12 @@
-import {React, useState} from "react";
+import {React} from "react";
 import SideBar from "../components/SideBar/SideBar";
 
-import { ListGroupItem } from "react-bootstrap";
-import SideBarItem from "../components/SideBar/SideBarItem";
-
-
 const AdminPanel = () => {
-
-    const [itemAdminPanel, setItemAdminPanel] = useState([
-        {
-            id: 1,
-            newLink: '/admin',
-            title: "Админ 1",
-        },
-        {
-            id: 2,
-            newLink: '/admin',
-            title: "Админ 2",
-        },
-        {
-            id: 3,
-            newLink: '/admin',
-            title: "Админ 3",
-        }
-    ])
-
-    const [newActive, setNewActive] = useState(false)
-
-
-
-    const onSelectActive = (id, active) => {
-        
-
-        const newItem = itemAdminPanel.map((itemAdmin) => 
-            itemAdmin.id === id ? {...itemAdmin, active} : itemAdmin
-        )
-        setItemAdminPanel(newItem)
-        console.log(newItem)
-        console.log(itemAdminPanel)
-         
-    }
 
     return (
         <div>
             <SideBar>
-                               
-                {itemAdminPanel.map((item) => (
-                    <SideBarItem key={item.id} {...item} onActiveClick = {active => onSelectActive()} newLink = {item.newLink} active={item.title}>
-                        {item.title}
-                    </SideBarItem>
-                ))}
-
-                
+                fgdfg
             </SideBar>
         </div>
     )
