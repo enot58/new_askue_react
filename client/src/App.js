164068e44b2cbf14dcbe22e5f4ad423575e7fe57
Roman Home <t89875041999@yandex.ru>
2022-10-02 21:53:28 +0300

    Changes to be committed: 	modified:   client/src/App.js 	deleted:    client/src/components/SideBar/SideBar.jsx 	deleted:    client/src/components/SideBar/SideBar.module.css 	deleted:    client/src/components/SideBar/SideBarItem.jsx 	deleted:    client/src/components/SideBar/SideBarItem.module.css 	deleted:    client/src/components/adminPanelItem/AdminPanelItem.jsx 	deleted:    client/src/components/adminPanelItem/AdminPanelItem.module.css 	deleted:    client/src/components/formInput.jsx 	deleted:    client/src/components/listItem.jsx 	deleted:    client/src/components/oneItem.jsx 	deleted:    client/src/components/test/formInput.jsx 	deleted:    client/src/components/test/listItem.jsx 	deleted:    client/src/components/test/oneItem.jsx 	deleted:    client/src/components/test/test.jsx 	deleted:    client/src/pages/AdminPanel.jsx 	deleted:    client/src/pages/Configured.jsx 	modified:   client/src/routes.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import NavBar from "./components/NavBar";
 import AppRouter from "./components/AppRouter";
 
 
+
 function App() {
   return (
     <BrowserRouter>
         <NavBar />
         <AppRouter />
+        
     </BrowserRouter>
   );
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,28 +1,29 @@
 import {
     MAIN_ROUTE,
-    CONFIGURED_ROUTE,
-    ADMIN_ROUTE
 } from "./utils/consts";
 
 
 import Main from "./pages/Main.jsx";
-import Configured from "./pages/Configured.jsx";
-import AdminPanel from "./pages/AdminPanel";
 
+
+
+
+
+
+export const authRoutes = [
+    {
+        path: MAIN_ROUTE,
+        Component: Main
+    },
+    
+]
 
 export const publickRoutes = [
     {
         path: MAIN_ROUTE,
         Component: Main
     },
-    {
-        path: CONFIGURED_ROUTE,
-        Component: Configured
-    },
-    {
-        path: ADMIN_ROUTE,
-        Component: AdminPanel
-    },
+    
 ]
 
 
